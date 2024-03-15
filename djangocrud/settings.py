018838872357caaae1394d6bfd1c764b21962c70
alonns04 <claudiogabrielalonso@gmail.com>
2024-03-14 22:27:22 -0300

    .

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -26,10 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
-#DEBUG = 'RENDER' not in os.environ
-
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
@@ -94,7 +91,7 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost/postpython manage.py collectstaticgres',
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
 }
