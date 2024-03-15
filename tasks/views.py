9d8b138a07381a276a3f7d846766e3f11f17e3e4
alonns04 <claudiogabrielalonso@gmail.com>
2024-03-15 13:55:12 -0300

    basic hw 0.6

--- tasks/views.py
+++ tasks/views.py
@@ -69,7 +69,17 @@ def create_task(request):
 
 @login_required
 def task_detail(request, task_id):
-    return render(request, 'task_detail.html ')  
+    task = get_object_or_404(Task, pk=task_id,  user=request.user)
+    if request.method == 'GET':   
+        form = TaskForm(instance=task)
+        return render(request, 'task_detail.html', {'task':task, 'form':form})    
+    else:
+        try: 
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'task_detail.html',{'task':task, 'form':form, 'error': 'Error Updating Task'})    
 
 @login_required 
 def complete_task(request, task_id):
@@ -106,4 +116,5 @@ def signin(request):
         else: 
             login(request, user)
             return redirect('tasks')
+            
 
